892dd8e7d7559710d39ec6ea94579445d63d84dd
eren12122 <44553580+eren12122@users.noreply.github.com>
2018-11-27 18:50:09 +0300

    Update ping.js

--- komutlar/ping.js
+++ komutlar/ping.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 exports.run = function(client, message, args) {
-  message.reply('Ping Değerim: **' + client.ping + '** ms');
+  message.reply('2.panel Ping Değerim: **' + client.ping + '** ms');
 };
 
 exports.conf = {
